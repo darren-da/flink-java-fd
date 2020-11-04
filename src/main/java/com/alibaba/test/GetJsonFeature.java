e50943dd43d110f2809179556b0372b4e081fe80
fada.yu <fada.yu@zhaopin.com.cn>
2020-11-04 19:45:02 +0800

    u

---
+++ src/main/java/com/alibaba/test/GetJsonFeature.java
@@ -0,0 +1,97 @@
+package com.alibaba.test;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Set;
+
+/**
+ * @author fada.yu
+ * @date 2020/10/28 13:31
+ * @Description：
+ */
+public class GetJsonFeature {
+//    private static final String URL = "https://zlfs-pre-api.***.com/feature/all";
+    private static final String URL = "http://d-feature-service.***.com/manager/loadKeyMapping?useCache=false";
+
+    /**
+     * 通过网络访问json并读取文件
+     *
+     * @param url:http://127.0.0.1:80/dashboard/dept_uuid.json
+     * @return:json文件的内容
+     */
+    public static String loadJson(String url) {
+        StringBuilder json = new StringBuilder();
+        try {
+            URL urlObject = new URL(url);
+            URLConnection uc = urlObject.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(), "UTF-8"));
+            String inputLine = null;
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json.toString();
+    }
+
+    /**
+     * 通过本地文件访问json并读取
+     *
+     * @param path：E:/svn/05.Hospital/templatedept_uuid.json
+     * @return：json文件的内容
+     */
+    public static String ReadFile(String path) {
+        String laststr = "";
+        File file = new File(path);// 打开文件
+        BufferedReader reader = null;
+        try {
+            FileInputStream in = new FileInputStream(file);
+            reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));// 读取文件
+            String tempString = null;
+            while ((tempString = reader.readLine()) != null) {
+                laststr = laststr + tempString;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException el) {
+                }
+            }
+        }
+        return laststr;
+    }
+
+
+    public static void main(String[] args) {
+        String featureJson = loadJson(URL);
+        JSONObject jsonObject = JSON.parseObject(featureJson).getJSONObject("data");
+
+        Set<String> features = jsonObject.keySet();
+
+        System.out.println("现所有feature个数：" + features.size());
+        for (String feature : features) {
+//            System.out.println("feature:"+feature+"\t"+"desc:"+jsonObject.getString(feature));
+            if (feature.startsWith("user") || feature.startsWith("jd") || feature.startsWith("cv") ||
+                    feature.startsWith("relation")) {
+                System.out.println(feature);
+            }
+
+
+        }
+
+
+    }
+}
--- src/main/java/com/alibaba/test/ResloveJson.java
+++ src/main/java/com/alibaba/test/ResloveJson.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.Iterator;
+
 /**
  * @author fada.yu
  * @date 2020/10/22 9:44
@@ -12,15 +14,74 @@ import com.alibaba.fastjson.JSONObject;
 public class ResloveJson {
     public static void main(String[] args) {
 
-       String jsonStr="{\"name\":\"胡小威\" , \"age\":20 , \"male\":true}";
-        JSONObject jsonObject = JSON.parseObject(jsonStr);
-//        System.out.println(jsonObject.getString("name"));
-        JSONObject jsonObject1 = new JSONObject(jsonObject);
-//        System.out.println(jsonObject1.getString("name"));
+      /*  {
+            "cluster_name":"21test",
+                "hearth":"true",
+                "nodes":{
+            "abc":{
+                "ip":"192.168.200.191",
+                        "version":"2.1.1"
+            },
+            "def":{
+                "ip":"192.168.200.191",
+                        "version":"2.1.1"
+            },
+            "ghi":{
+                "ip":"192.168.200.196",
+                        "version":"2.1.1"
+            }
+        }
+        }*/
 
 
+        String jsonStr = "{\"cluster_name\":\"21test\",\"hearth\":\"true\",\"nodes\":{\"abc\":{\"ip\":\"192.168.200.191\",\"version\":\"2.1.1\"},\"def\":{\"ip\":\"192.168.200.191\",\"version\":\"2.1.1\"},\"ghi\":{\"ip\":\"192.168.200.196\",\"version\":\"2.1.1\"}}}";
+        JSONObject jsonObj = JSONObject.parseObject(jsonStr);
+        System.out.println(jsonObj.getString("cluster_name"));
+        System.out.println(jsonObj.getString("nodes"));
+        System.out.println(jsonObj.getJSONObject("nodes").size());
+//        System.out.println(jsonObj.getJSONObject("nodes").isEmpty());
+//        System.out.println(jsonObj.getJSONObject("nodes").getJSONObject("def"));
+        System.out.println(jsonObj.getJSONObject("nodes").getString("def"));
 
+        JSONObject nodes = jsonObj.getJSONObject("nodes");
+        Iterator<String> it = nodes.keySet().iterator();
 
+       while (it.hasNext()){
+           String key = it.next();
+           System.out.println(key + ":" + nodes.getJSONObject(key).getString("ip") + " " +"version:"+ nodes.getJSONObject(key).getString("version"));
+
+       }
+
+       //测试get JsonArray()
+        String arrayJson = "{\"info\":[{\"goodsId\":\"1234\",\"goodsq\":\"10\"},{\"goodsId\":\"5678\",\"goodsq\":\"20\"}]}";
+       testArray(arrayJson);
+ /*
+        {
+          "info": [
+            {
+              "goodsId": "1234",
+              "goodsq": "10"
+            },
+            {
+              "goodsId": "5678",
+              "goodsq": "20"
+            }
+          ]
+        }
+    */
+
+//json数组
+
+    }
+
+    public static void testArray(String jsonStr){
+        JSONObject jsonObject = JSONObject.parseObject(jsonStr);
+        JSONArray arrayJson = jsonObject.getJSONArray("info");
+        for (int i = 0; i <arrayJson.size() ; i++) {
+            JSONObject arrayMode = arrayJson.getJSONObject(i);
+            System.out.println(arrayMode.getString("goodsId"));
+
+        }
 
     }
 }
