2abe1271d3d9c23573b320559b873cc455280072
fada.yu <fada.yu@zhaopin.com.cn>
2021-03-13 10:00:06 +0800

    u

---
+++ src/main/java/com/alibaba/base/bean/WaterSensors.scala
@@ -0,0 +1,14 @@
+package com.alibaba.base.bean
+
+/**
+  * @author :YuFada
+  * @date ： 2021/3/13 0013 上午 9:30
+  *       Description：
+  */
+case class WaterSensors (
+                            id: String, ts: Long, vc: Integer
+                        )
+
+
+
+
