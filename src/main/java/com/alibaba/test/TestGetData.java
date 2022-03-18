fd502a2e3178653bd02318dc774cbbd3d21f36d5
fada.yu <fada.yu@zhaopin.com.cn>
2022-03-18 18:48:10 +0800

    u

--- src/main/java/com/alibaba/test/TestGetData.java
+++ src/main/java/com/alibaba/test/TestGetData.java
@@ -34,11 +34,9 @@ public class TestGetData {
         //modelData=modelData?.get(propertyPath[i]) 是2
         //modelData?.getRowModel(modelData.getFocusedRowIndex()) 上一位是2，下一位不是2
 
-        String[] pathDesc= "2.1".split("\\.");
-        String[] propertyPath = "freeDetail001.productId_name".split("\\.");
-
+        String[] pathDesc= "2.2.1".split("\\.");
+        String[] propertyPath = "freeDetail001.freeDetail002.productId_name".split("\\.");
         //modelData?.get(freeDetail001).getRowModel(modelData.getFocusedRowIndex()).get(p1freeCharacteristics).get(TEST_USER_30)
-
         String modelData="";
         for (int i = 0; i <pathDesc.length ; i++) {
             if (i==0){
@@ -47,42 +45,26 @@ public class TestGetData {
             }else {
                 if (pathDesc[i-1].equals("2") && !pathDesc[i].equals("2")){
                     //modelData="modelData?.getRowModel(modelData.getFocusedRowIndex()).get(propertyPath[i])";
-                    modelData+=".getRowModel(modelData.getFocusedRowIndex()).get("+propertyPath[i]+")";
+                    modelData+=".getRowModel(modelData.getFocusedRowIndex()+1).get("+propertyPath[i]+")";
                 }else  {
                     //modelData="modelData?.get(propertyPath[i])";
                     modelData+=".get("+propertyPath[i]+")";
-
                 }
             }
         }
-
-/*        for (int i = 1; i <pathDesc.length ; i++) {
-
-            //    //modelData="modelData?.get(propertyPath[i])";
-                modelData="modelData?.get("+propertyPath[i-1]+")";
-                if (pathDesc[i-1].equals("2") && !pathDesc[i].equals("2")){
-            //        //modelData="modelData?.getRowModel(modelData.getFocusedRowIndex()).get(propertyPath[i])";
-                    modelData+=".getRowModel(modelData.getFocusedRowIndex()).get("+propertyPath[i]+")";
-                }else  {
-            //        //modelData="modelData?.get(propertyPath[i])";
-                    modelData+=".get("+propertyPath[i]+")";
-            //
-                }
-            }*/
-
         System.out.println("modelData= "+modelData);
     }
 }
 //const propertyPath = item?.propertyPath?.split('.')
 //        const pathDesc = item?.pathDesc?.split('.')
-//        for(let i = 0; i　< pathDesc.length; i++) {
-//            if(i==0){
-//        modelData = modelData?.get(propertyPath[i])
-//        }else{
-//            if (pathDesc[i-1]==2 && pathDesc[i]!=2) {
-//            modelData = modelData?.getRowModel(modelData.getFocusedRowIndex())
-//            } else {
-//            modelData = modelData?.get(propertyPath[i])
-//            }
-//        }
-//        }
+//   for(let i = 0; i　< pathDesc.length; i++) {
+//              if(i==0){
+//                modelData = modelData?.get(propertyPath[i])
+//              }else{
+//                if (pathDesc[i-1]==2 && pathDesc[i]!=2) {
+//                  modelData = modelData?.getRowModel(modelData.getFocusedRowIndex()+1).get(propertyPath[i])
+//                } else {
+//                  modelData = modelData?.get(propertyPath[i])
+//                }
+//              }
+//            }
