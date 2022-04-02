433fa82456275070c19a9026852aeacda8122636
fada.yu <fada.yu@zhaopin.com.cn>
2022-04-02 19:24:37 +0800

    u

---
+++ src/main/java/com/alibaba/test/Java8.java
@@ -0,0 +1,59 @@
+package com.alibaba.test;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author fada.yu
+ * @version 1.0
+ * @date 2022/4/2 18:53
+ * @Desc:
+ * java 8新特性探研
+ */
+public class Java8 {
+    /**
+     * java7和java8比较排序
+     */
+    @Test
+    public void sortCompare() {
+        List<String> names1 = new ArrayList<String>();
+        names1.add("Google ");
+        names1.add("Runoob ");
+        names1.add("Taobao ");
+        names1.add("Baidu ");
+        names1.add("Sina ");
+
+        List<String> names2 = new ArrayList<String>();
+        names2.add("Google ");
+        names2.add("Runoob ");
+        names2.add("Taobao ");
+        names2.add("Baidu ");
+        names2.add("Sina ");
+
+        Java8 tester = new Java8();
+        System.out.println("使用 Java 7 语法: ");
+
+        tester.sortUsingJava7(names1);
+        System.out.println(names1);
+        System.out.println("使用 Java 8 语法: ");
+
+        tester.sortUsingJava8(names2);
+        System.out.println(names2);
+    }
+
+    // 使用 java 7 排序
+    private void sortUsingJava7(List<String> names){
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+    }
+
+    // 使用 java 8 排序
+    private void sortUsingJava8(List<String> names){
+        Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+    }
+}
